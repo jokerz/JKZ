8541589f5336547220ed51137b4c2f422075e498
Ryo Iwahase <jokerz.org@gmail.com>
2015-10-05 16:26:40 +0900

    半角全角変化処理を追加

--- JKZ/MyClass/WebUtil.pm
+++ JKZ/MyClass/WebUtil.pm
@@ -48,6 +48,7 @@
 # * @update        2010/10/27 convertByNKF文字コード処理を配列に対応
 # * @update        2010/12/10 subtractDateTimeAFromDateTimeB関数追加
 # * @update        2011/10/31 encodeUriとdecodeUri関数の追加
+# * @update         2015/10/05 半角全角変換の新処理を追加
 #********************************************
 package MyClass::WebUtil;
 
@@ -63,6 +64,34 @@ use NKF;
 #////////////////////////////////////////////
 # 文字列処理関係
 #////////////////////////////////////////////
+
+{
+  use utf8;
+  use charnames ':full';                                                                          
+  use Unicode::Normalize;                                                                         
+  
+  my $hankaku = "\x{FF9E}\x{FF9F}";                                                               
+  my $zenkaku = "\x{3099}\x{309A}";
+  
+  for my $o (0xFF61 .. 0xFF9D) {
+    $hankaku .= chr $o;
+    my $n = charnames::viacode($o);
+    $n =~ s/HALFWIDTH\s+//;
+    $zenkaku .= chr charnames::vianame($n);
+  }
+  
+  *tr_h2z = eval "sub { local \$_ = shift; tr/$hankaku/$zenkaku/; \$_}";
+  *tr_z2h = eval "sub { local \$_ = shift; tr/$zenkaku/$hankaku/; \$_}";
+  
+  sub han2zen { NFC(tr_h2z(shift)) }
+  sub zen2han { NFC(tr_z2h(NFD(shift))) }
+  
+  sub hira2kata {
+    local $_ = shift;
+    tr/\x{3041}-\x{3096}/\x{30A1}-\x{30F6}/;
+    $_;
+  }
+}  
 
 #********************************************
 # サニタイズ
